b4e81a266e669b889849eaa0a7e857b9d243d446
Ariday Bordon <aridaybordon@gmail.com>
2022-02-02 11:09:04 +0000

    enhancement: Allow return in EUR

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
 import requests
 import json
 
+import yfinance as yf
 
-def check_address_balance(address: str, return_in_satoshis: bool=False) -> float:
-    # Return address' balance for an account in bitcoin (default) or in satoshis.
+
+def load_addresses() -> list:
+    # Load all addresses in address.txt
+    with open('address.txt', 'r') as file:
+        return file.read().split()
+
+
+def get_address_balance(address: str, return_btc: bool=False, return_eur: bool=False) -> float:
+    # Return address' balance for an account in satoshis (default), bitcoin or eur.
     r       = requests.get(f"https://blockchain.info/rawaddr/{address}")
     data    = json.loads(r.text)
 
-    if "final_balance" in data.keys():
-        balance = data['final_balance'] if return_in_satoshis else data['final_balance']/1e8
-        return balance
-    raise ValueError("'address' is not valid or have not been found")
+    if not 'final_balance' in data.keys():
+        raise ValueError("'address' is not valid or have not been found")
+
+    balance = data['final_balance']
+
+    if return_btc:
+        return balance/1e8
+
+    if return_eur:
+        btc_value = yf.Ticker('BTC-EUR').history(period='1d')['Close'][0]
+        return balance*btc_value/1e8
+    
+    return balance
+
+
+def get_total_balance(return_btc: bool=False, return_eur: bool=False) -> float:
+    # Get total balance for all addresses in address.txt
+    balance = 0
+    for address in load_addresses():
+        balance += get_address_balance(address, return_btc, return_eur)
+    return balance
+
+
+if __name__ == '__main__':
+    print(get_total_balance(return_eur=True))
