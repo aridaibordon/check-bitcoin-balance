3e29a05b5dab4e7dbff2fc2779d3998c386b3a62
Aridai Bordón <aridaybordon@gmail.com>
2022-06-13 08:25:44 +0100

    update: create package layout

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name = "bitcoin_balance",
+    version = "0.0.1",
+    author = "Aridai Bordón",
+    description = "Check the balance of any bitcoin address",
+    packages=['yfinance'],
+    keywords=['bitcoin', 'crypto'],
+    download_url='https://github.com/aridaybordon/check-bitcoin-balance',
+    license='MIT'
+)
