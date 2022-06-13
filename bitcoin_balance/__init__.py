b9ac3ff56eb46c303509b8cd0c45111efc74139e
Aridai Bordón <aridaybordon@gmail.com>
2022-06-13 08:26:15 +0100

    enhancement: read addresses from private key

--- bitcoin_balance/__init__.py
+++ bitcoin_balance/__init__.py
@@ -1,43 +1,61 @@
 import requests
 import json
+import time
 
 import yfinance as yf
 
+from bitcoin_balance.private import get_public_address_from_private_key
 
-def load_addresses() -> list:
-    # load all addresses in address.txt
-    with open('address.txt', 'r') as file:
-        return file.read().split()
+ADDRESS_FILENAME = 'address.txt'
 
 
-def add_address(address) -> None:
-    # add new address to address.txt
-    with open('address.txt', 'r') as file:
+def load_addresses(filename: str = ADDRESS_FILENAME) -> list:
+    """Load addresses on ADDRESS_FILENAME"""
+    try:
+        with open('address.txt', 'r') as file:
+            return file.read().split()
+    except FileNotFoundError as e:
+        print(f"{ADDRESS_FILENAME} has not been created yet.")
+        return []
+
+
+def add_address(address, filename: str = ADDRESS_FILENAME) -> None:
+    """Add address to ADDRESS_FILENAME"""
+    with open(ADDRESS_FILENAME, 'r') as file:
         file.write(address)
 
 
-def get_address_balance(address: str, return_btc: bool=False, return_eur: bool=False) -> float:
-    # return address' balance for an account in satoshis (default), bitcoin or eur.
-    r       = requests.get(f"https://blockchain.info/rawaddr/{address}")
-    data    = json.loads(r.text)
+def get_address_balance(address: str,
+                        return_btc: bool = False,
+                        return_dol: bool = False,
+                        private_key: bool = False) -> float:
+    """Return balance for a given BTC address"""
+    r = requests.get(f"https://blockchain.info/rawaddr/{address}")
+    data = json.loads(r.text)
 
     if not 'final_balance' in data.keys():
-        raise ValueError(f"'address' {address} is not valid or have not been found")
-
-    balance = data['final_balance']/1e8 if return_btc else data['final_balance']
-
-    if return_eur:
-        btc_value = yf.Ticker('BTC-EUR').history(period='1d')['Close'][0]
-        return balance*btc_value/1e8
+        raise ValueError(
+            f"'address' {address} is not valid or have not been found")
     
+    if private_key:
+        address = get_public_address_from_private_key(address)
+
+    balance = data['final_balance'] / 1e8 if return_btc else data[
+        'final_balance']
+
+    if return_dol:
+        btc_value = yf.Ticker('BTC-USD').history(period='1d')['Close'][0]
+        return balance * btc_value / 1e8
+
     return balance
 
 
-def get_total_balance(return_sum: bool=True, return_btc: bool=False, return_eur: bool=False) -> float:
-    # get total balance for all addresses in address.txt
+def get_total_balance(return_sum: bool = True, **kwargs) -> float:
+    """Return total balance of all addresses in ADDRESS_FILENAME"""
     balance = {}
     for address in load_addresses():
-        balance[address] = get_address_balance(address, return_btc, return_eur)
+        balance[address] = get_address_balance(address, **kwargs)
+        time.sleep(1)
 
     if return_sum:
         return sum(balance.values())
---
+++ bitcoin_balance/private.py
@@ -0,0 +1,39 @@
+import ecdsa
+import hashlib
+import base58
+
+def get_public_address_from_private_key(private_key: str):
+    """ Return the public address from a private key
+
+    Parameters
+    ----------
+    * private_key: string with the private key.
+
+    Original code from Shlomi Zeltsinger (https://www.youtube.com/watch?v=tX-XokHf_nI).
+
+    """
+    # WIF to private key by https://en.bitcoin.it/wiki/Wallet_import_format
+    private_key = base58.b58decode_check(private_key) 
+    private_key = private_key[1:]
+
+    # Private key to public key (ecdsa transformation)
+    signing_key = ecdsa.SigningKey.from_string(private_key, curve = ecdsa.SECP256k1)
+    verifying_key = signing_key.get_verifying_key()
+    public_key = bytes.fromhex("04") + verifying_key.to_string()
+
+    # hash sha 256 of pubkey
+    sha256_1 = hashlib.sha256(public_key)
+
+    # hash ripemd of sha of pubkey
+    ripemd160 = hashlib.new("ripemd160")
+    ripemd160.update(sha256_1.digest())
+
+    # checksum
+    hashed_public_key = bytes.fromhex("00") + ripemd160.digest()
+    checksum_full = hashlib.sha256(hashlib.sha256(hashed_public_key).digest()).digest()
+    checksum = checksum_full[:4]
+    bin_addr = hashed_public_key + checksum
+
+    # encode address to base58 and print
+    public_address = base58.b58encode(bin_addr)
+    return public_address.decode("utf-8")
